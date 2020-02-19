7f275f0d7e8fb91ee57a77e004b519b7803aa849
Yi Shen <shen1@ualberta.ca>
2020-02-19 18:52:08 -0500

    Password validation implemented, need better jump dest

--- BackEnd/demoapp/registration/views.py
+++ BackEnd/demoapp/registration/views.py
@@ -2,9 +2,8 @@ from django.shortcuts import render
 from django.contrib.staticfiles.views import serve
 from django.http import HttpResponseRedirect
 from django.urls import reverse
-from django.contrib.auth.models import User
 from django.views.generic.edit import CreateView
-
+from .forms import UserForm
 # Create your views here.
 def redirect(request):
     return HttpResponseRedirect(reverse('registration:login'))
@@ -12,5 +11,4 @@ def signup(request):
     return render(request, 'registration/signup.html')
 class UserCreate(CreateView):
     template_name = 'registration/user_form.html'
-    model = User
-    fields = ['username', 'password','email','first_name', 'last_name']
+    form_class  = UserForm
